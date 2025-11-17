63e0c38de6f7cf6d51108470f79c28b9410efb9c
fnielson1 <f.nielson@gmail.com>
2025-11-17 13:34:12 -0700

    feat(setup): setup to have vercel call my hello function

--- packages/backend/hello.ts
+++ packages/backend/hello.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getGreeting } from '../src/greet';
+import { getGreeting } from './src/greet';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).json({ message: getGreeting() });
